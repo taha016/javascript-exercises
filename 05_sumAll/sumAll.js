a90a509689f963f26c8b3ba06027b477d61bd4d4
taha016 <msaddiqueshareef@gmail.com>
2024-12-23 22:39:25 +0500

    solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(st, end) {
+    if(st > end) {
+        st = st + end;
+        end = st - end;
+        st = st - end;
+    }
+    if(st < 0 || end < 0) return "ERROR";
 
+    if(!Number.isInteger(st) || !Number.isInteger(end)) return "ERROR";
+    let finalSum = 0;
+    for(let i = st; i <= end; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
